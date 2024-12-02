dcd55dbcad8fa002a5581be82f19155d963e323b
techtoto <toto.rei0210@gmail.com>
2024-12-02 16:21:39 +0100

    add part 1 of day 2

---
+++ day02/day02.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+reports = []
+safe_reports = 0
+
+def whatamidoing(x):
+    return list(map(lambda x : int(x), x.split(" ")))
+
+with open("./day02/day02-input.txt") as f:
+    reports = list(map(whatamidoing, f.read().splitlines()))
+
+for report in reports:
+    sorted_ascending = report.copy()
+    sorted_descending = report.copy()
+    sorted_ascending.sort()
+    sorted_descending.sort(reverse = True)
+
+    if report != sorted_ascending and report != sorted_descending:
+        continue
+
+    safe = True
+    for i in range(len(report) - 1):
+        if abs(report[i] - report[i+1]) < 1 or abs(report[i] - report[i+1]) > 3:
+            safe = False
+            break
+    
+    if safe:
+        safe_reports += 1
+
+print(safe_reports)
