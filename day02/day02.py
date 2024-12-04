0077ff88f246d7271ba18c673b70db5bf4b3af48
Tobias Reichenbach <toto.rei0210@gmail.com>
2024-12-04 11:24:13 +0100

    add solution for day 2 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 
 reports = []
-safe_reports = 0
+safe_reports_1 = 0
+safe_reports_2 = 0
 
 with open("./day02/day02-input.txt") as f:
     reports = [[int(x) for x in line.split(" ")] for line in f.read().splitlines()]
 
-for report in reports:
+def is_safe(report):
     sorted_ascending = report.copy()
     sorted_descending = report.copy()
     sorted_ascending.sort()
     sorted_descending.sort(reverse = True)
 
     if report != sorted_ascending and report != sorted_descending:
-        continue
+        return False
 
     safe = True
     for i in range(len(report) - 1):
         if abs(report[i] - report[i+1]) < 1 or abs(report[i] - report[i+1]) > 3:
             safe = False
             break
-    
-    if safe:
-        safe_reports += 1
+    return safe
 
-print(safe_reports)
+for report in reports:
+    if is_safe(report):
+        safe_reports_1 += 1
+    else:
+        for i in range(len(report)):
+            i_removed = report.copy()
+            i_removed.pop(i)
+            if is_safe(i_removed):
+                safe_reports_2 += 1
+                break
+
+print(safe_reports_1)
+print(safe_reports_1 + safe_reports_2)
