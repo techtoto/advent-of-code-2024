c725a77e022e026abd4b20e028ac7f6ee25e3925
Tobias Reichenbach <toto.rei0210@gmail.com>
2024-12-07 12:38:47 +0100

    add solution for day 7 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-import re
+import re, numpy
 
 with open("./day07/day07-input.txt") as f:
     lines = re.findall(r"(\d+):\s(.+)", f.read())
 
-current_sum = 0
+sum_1 = 0
+sum_2 = 0
 
 for line in lines:
     expected = int(line[0])
     numbers = [int(x) for x in line[1].split(" ")]
     
+    # PART 1
+    # every permutation of additions and multiplications: 2^(len(numbers) - 1)
     # n acts as a bitmask; 0b0 -> add, 0b1 -> multiply
-    for n in range(1 << (len(numbers) - 1)): # every permutation of additions and multiplications: 2^(len(numbers) - 1)
+    for n in range(1 << (len(numbers) - 1)):
         result = numbers[0]
         for i in range(1, len(numbers)):
             if ((n >> (i - 1)) & 1) == 1:
@@ -19,7 +22,24 @@ for line in lines:
             else:
                 result += numbers[i]
         if result == expected:
-            current_sum += result
+            sum_1 += result
             break
 
-print(current_sum)
+    # PART 2
+    # every permutation of additions, multiplications and concatenations: 3^(len(numbers) - 1)
+    for n in range(3 ** (len(numbers) - 1)):
+        result = numbers[0]
+        n_base_3 = numpy.base_repr(n, 3).zfill(len(numbers) - 1)
+        for i in range(1, len(numbers)):
+            if n_base_3[i - 1] == '1':
+                result *= numbers[i]
+            elif n_base_3[i - 1] == '0':
+                result += numbers[i]
+            else:
+                result = int(f"{result}{numbers[i]}")
+        if result == expected:
+            sum_2 += result
+            break
+
+print(sum_1)
+print(sum_2)
